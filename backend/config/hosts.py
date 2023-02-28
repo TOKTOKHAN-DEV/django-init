69f9849d39e553b5206a4e47fe6bbc12d7d8d0a4
devnine99 <46771072+devnine99@users.noreply.github.com>
2023-02-28 14:26:07 +0900

    initial commit

--- backend/config/hosts.py
+++ backend/config/hosts.py
@@ -4,6 +4,4 @@ host_patterns = patterns(
     "",
     host("api", "config.urls.api", name="api"),
     host("admin", "config.urls.admin", name="admin"),
-    host("api-dev", "config.urls.api", name="api-dev"),
-    host("admin-dev", "config.urls.admin", name="admin-dev"),
 )
