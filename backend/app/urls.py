d53b52fc0b31f06c8d344fb3f135fa6a43e03ed9
devnine99 <46771072+devnine99@users.noreply.github.com>
2023-04-03 10:42:00 +0900

    initial commit

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path("v1/", include("app.urls.v1")),
+    path("v1/", include("app.user.v1.urls")),
+    path("v1/", include("app.presigned_url.v1.urls")),
+    path("v1/", include("app.verifier.v1.urls")),
+    path("v1/", include("app.chat.v1.urls")),
 ]
