6ce7995c55264d1daa994ba2be4a0334f96e4b5c
devnine99 <46771072+devnine99@users.noreply.github.com>
2023-07-11 15:26:44 +0900

    main

--- backend/app/user/v1/views.py
+++ backend/app/user/v1/views.py
@@ -20,7 +20,7 @@ from app.user.v1.serializers import (
 
 
 @extend_schema_view(
-    me=extend_schema(summary="유저 조회"),
+    retrieve=extend_schema(summary="유저 조회"),
     login=extend_schema(summary="유저 로그인"),
     social_login=extend_schema(summary="유저 소셜 로그인"),
     logout=extend_schema(summary="유저 로그아웃"),
@@ -33,30 +33,14 @@ class UserViewSet(
     GenericViewSet,
 ):
     queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_value_regex = ".+"
 
-    def get_serializer_class(self):
-        if self.action == "me":
-            return UserSerializer
-        if self.action == "login":
-            return UserLoginSerializer
-        if self.action == "social_login":
-            return UserSocialLoginSerializer
-        if self.action == "logout":
-            return UserLogoutSerializer
-        if self.action == "refresh":
-            return TokenRefreshSerializer
-        if self.action == "register":
-            return UserRegisterSerializer
-        if self.action == "password_reset":
-            return UserPasswordResetSerializer
-        if self.action == "password_reset_confirm":
-            return UserPasswordResetConfirmSerializer
-        raise Exception
-
-    def get_permissions(self):
-        if self.action in ["me", "logout"]:
-            return [IsAuthenticated()]
-        return []
+    def get_object(self):
+        if self.kwargs.get("pk") == "me":
+            return self.queryset.get(id=self.request.user.id)
+        return super().get_object()
 
     def _create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -64,16 +48,11 @@ class UserViewSet(
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    @action(methods=["GET"], detail=False)
-    def me(self, request, *args, **kwargs):
-        serializer = self.get_serializer(self.request.user)
-        return Response(serializer.data)
-
-    @action(methods=["POST"], detail=False)
+    @action(methods=["POST"], detail=False, serializer_class=UserLoginSerializer, permission_classes=[])
     def login(self, request, *args, **kwargs):
         return self._create(request, *args, **kwargs)
 
-    @action(methods=["POST"], detail=False)
+    @action(methods=["POST"], detail=False, serializer_class=UserSocialLoginSerializer, permission_classes=[])
     def social_login(self, request, *args, **kwargs):
         """
         ![social_login_flow]({settings.STATIC_URL}docs/social_login_flow.jpeg)
@@ -96,14 +75,14 @@ class UserViewSet(
         """
         return self._create(request, *args, **kwargs)
 
-    @action(methods=["POST"], detail=False)
+    @action(methods=["POST"], detail=False, serializer_class=UserLogoutSerializer)
     def logout(self, request, *args, **kwargs):
         """
         모바일앱에서만 사용하며, 유저와 디바이스 토큰의 연결을 끊어주기위해 사용합니다.
         """
         return self._create(request, *args, **kwargs)
 
-    @action(methods=["POST"], detail=False)
+    @action(methods=["POST"], detail=False, serializer_class=TokenRefreshSerializer, permission_classes=[])
     def refresh(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         try:
@@ -112,21 +91,21 @@ class UserViewSet(
             raise InvalidToken(e.args[0])
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
-    @action(methods=["POST"], detail=False)
+    @action(methods=["POST"], detail=False, serializer_class=UserRegisterSerializer, permission_classes=[])
     def register(self, request, *args, **kwargs):
         """
         소셜 유저인 경우 `socialToken`을 전달해야합니다.
         """
         return self._create(request, *args, **kwargs)
 
-    @action(methods=["POST"], detail=False)
+    @action(methods=["POST"], detail=False, serializer_class=UserPasswordResetSerializer, permission_classes=[])
     def password_reset(self, request, *args, **kwargs):
         """
         이메일을 통해 비밀번호 재설정 가능한 link을 발급받습니다.
         """
         return self._create(request, *args, **kwargs)
 
-    @action(methods=["POST"], detail=False)
+    @action(methods=["POST"], detail=False, serializer_class=UserPasswordResetConfirmSerializer, permission_classes=[])
     def password_reset_confirm(self, request, *args, **kwargs):
         """
         유저 비밀번호 초기화 메일 발송 API를 통해 발급 받은 link를 통해 비밀번호를 재설정합니다.
