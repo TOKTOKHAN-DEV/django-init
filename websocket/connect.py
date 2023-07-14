8e8217a5d9fb2417b7f3e18574af383af143323b
devnine99 <46771072+devnine99@users.noreply.github.com>
2023-07-14 18:46:38 +0900

    main

--- websocket/connect.py
+++ websocket/connect.py
@@ -18,9 +18,13 @@ env = os.getenv("ENV")
 
 def handler(event, context):
     user_id = 0
-    access = event.get("queryStringParameters") and event["queryStringParameters"].get("access")
+    access = event.get("queryStringParameters") and event["queryStringParameters"].get(
+        "access"
+    )
     if access:
-        response = secretmanager.get_secret_value(SecretId=f"{project_name}/django/{env}")
+        response = secretmanager.get_secret_value(
+            SecretId=f"{project_name}/django/{env}"
+        )
         secret = json.loads(response["SecretString"])
         payload = jwt_decode(access, secret["key"])
         user_id = payload["user_id"]
--- websocket/disconnect.py
+++ websocket/disconnect.py
@@ -6,9 +6,6 @@ db = boto3.client("dynamodb")
 
 
 def lambda_handler(event, context):
-    print(event)
-    print(context)
-
     db.delete_item(
         TableName=os.getenv("TABLE_NAME"),
         Key={"connection_id": {"S": event["requestContext"]["connectionId"]}},
