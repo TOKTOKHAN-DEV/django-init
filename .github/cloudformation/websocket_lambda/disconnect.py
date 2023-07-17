14eaf21f292a3f475ba87103b86cd190ddeff1d7
devnine99 <46771072+devnine99@users.noreply.github.com>
2023-07-17 10:20:21 +0900

    main

--- .github/cloudformation/websocket_lambda/disconnect.py
+++ .github/cloudformation/websocket_lambda/disconnect.py
@@ -5,7 +5,7 @@ import boto3
 db = boto3.client("dynamodb")
 
 
-def lambda_handler(event, context):
+def handler(event, context):
     db.delete_item(
         TableName=os.getenv("TABLE_NAME"),
         Key={"connection_id": {"S": event["requestContext"]["connectionId"]}},
