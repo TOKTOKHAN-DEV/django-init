dd48b42e0a6e2a7f46932312ceafa92fd79c1ec9
devnine99 <46771072+devnine99@users.noreply.github.com>
2023-07-17 16:10:49 +0900

    main

--- backend/app/common/models.py
+++ backend/app/common/models.py
@@ -1,10 +1,7 @@
 from django.db import models
-from safedelete import SOFT_DELETE
-from safedelete.models import SafeDeleteModel
 
 
-class BaseModelMixin(SafeDeleteModel):
-    _safedelete_policy = SOFT_DELETE
+class BaseModelMixin(models.Model):
     created_at = models.DateTimeField(verbose_name="생성일시", auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name="수정일시", auto_now=True)
 
