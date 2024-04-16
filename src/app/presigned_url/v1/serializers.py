eb59fca122837683e35273c6f6193f6216693923
devnine99 <46771072+devnine99@users.noreply.github.com>
2024-04-16 10:11:56 +0900

    lambdaExcutionRole

--- src/app/presigned_url/v1/serializers.py
+++ src/app/presigned_url/v1/serializers.py
@@ -1,49 +1,60 @@
-import uuid
+import random
+import string
 
 import boto3
 from botocore.config import Config
 from botocore.exceptions import ClientError
-from django.apps import apps
 from django.conf import settings
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 
-class PresignedUrlSerializer(serializers.Serializer):
-    name = serializers.CharField(write_only=True)
-    category = serializers.ChoiceField(
-        choices=[model.__name__.lower() for model in apps.get_models() if model.__module__.startswith("app.")],
-        write_only=True,
-    )
+class PresignedSerializer(serializers.Serializer):
+    file_name = serializers.CharField(write_only=True)
     url = serializers.URLField(read_only=True)
+    fields = serializers.JSONField(read_only=True)
 
     def validate(self, attrs):
-        attrs["url"] = self.create_presigned_url(attrs["name"], attrs["category"])
+        response = self.create_presigned_post(attrs["file_name"])
+        attrs.update(response)
 
         return attrs
 
     def create(self, validated_data):
         return validated_data
 
-    @staticmethod
-    def create_presigned_url(name, category):
+    def create_presigned_post(self, file_name):
         s3_config = Config(
             region_name="ap-northeast-2",
             signature_version="s3v4",
         )
         s3_client = boto3.client("s3", config=s3_config)
-        ext = name.split(".")[-1]
+        basename = "/".join(["_media", self.context["view"].basename])
+        object_key = self.get_object_key(s3_client, basename, file_name)
 
-        object_key = "/".join(["_media/category", f"{uuid.uuid4()}.{ext}"])
+        response = s3_client.generate_presigned_post(
+            settings.AWS_STORAGE_BUCKET_NAME,
+            object_key,
+            Fields={"x-amz-tagging": "status=editing"},
+            Conditions=[
+                {"x-amz-tagging": "status=editing"},
+                ["content-length-range", 0, 20971520],  # 20MB
+                ["starts-with", "$Content-Type", f"image/"],
+            ],
+            ExpiresIn=360,
+        )
+        return response
+
+    def get_object_key(self, s3_client, basename, file_name):
+        object_key = f"{basename}/{file_name}"
         try:
-            url = s3_client.generate_presigned_url(
-                "put_object",
-                Params={
-                    "Bucket": settings.AWS_STORAGE_BUCKET_NAME,
-                    "Key": object_key,
-                },
-                ExpiresIn=300,
+            s3_client.head_object(
+                Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+                Key=object_key,
             )
-        except ClientError as e:
-            raise ValidationError({"s3": ["S3 Client Error"]})
-        return url
+            name, ext = file_name.split(".")
+            characters = string.ascii_lowercase + string.digits
+            random_string = "".join(random.choices(characters, k=7))
+            file_name = ".".join([f"{name}_{random_string}", ext])
+            return self.get_object_key(s3_client, basename, file_name)
+        except ClientError:
+            return object_key
