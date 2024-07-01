da589ec873b0180eba9591d36b522ac52bbb6442
devnine99 <46771072+devnine99@users.noreply.github.com>
2024-07-01 11:18:12 +0900

    main

--- src/app/websocket_connection/v1/serializers.py
+++ src/app/websocket_connection/v1/serializers.py
@@ -1,23 +1,39 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import AccessToken
 
 from app.websocket_connection.models import WebsocketConnection
 
 
 class WebsocketConnectionSerializer(serializers.ModelSerializer):
+    access_token = serializers.CharField(write_only=True)
+
     class Meta:
         model = WebsocketConnection
         fields = [
             "id",
+            "access_token",
         ]
 
     def validate(self, attrs):
-        attrs = super().validate(attrs)
+        if attrs.pop("access_token", None):
+            try:
+                token = AccessToken(token=attrs["access_token"])
+            except TokenError as e:
+                raise ValidationError(e)
+            attrs["user_id"] = token.payload["user_id"]
         return attrs
 
-    def create(self, validated_data):
-        instance = super().create(validated_data)
-        return instance
 
-    def update(self, instance, validated_data):
-        instance = super().update(instance, validated_data)
-        return instance
+class WebsocketDisconnectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WebsocketConnection
+        fields = ["id"]
+        extra_kwargs = {
+            "id": {"validators": []},
+        }
+
+    def create(self, validated_data):
+        WebsocketConnection.objects.filter(id=validated_data["id"]).delete()
+        return validated_data
--- src/app/websocket_connection/v1/views.py
+++ src/app/websocket_connection/v1/views.py
@@ -1,36 +1,43 @@
 from drf_spectacular.utils import extend_schema, extend_schema_view
-from rest_framework import mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from app.websocket_connection.models import WebsocketConnection
-from app.websocket_connection.v1.serializers import WebsocketConnectionSerializer
+from app.websocket_connection.v1.permissions import WebsocketConnectionPermission
+from app.websocket_connection.v1.serializers import WebsocketConnectionSerializer, WebsocketDisconnectionSerializer
 
 
 @extend_schema_view(
-    create=extend_schema(summary="WebsocketConnection 등록"),
-    destroy=extend_schema(summary="WebsocketConnection 삭제"),
+    connect=extend_schema(summary="WebsocketConnection 등록"),
+    disconnect=extend_schema(summary="WebsocketConnection 삭제"),
 )
 class WebsocketConnectionViewSet(
-    mixins.CreateModelMixin,
-    mixins.DestroyModelMixin,
     GenericViewSet,
 ):
     queryset = WebsocketConnection.objects.all()
     serializer_class = WebsocketConnectionSerializer
-    permission_classes = []
+    permission_classes = [WebsocketConnectionPermission]
 
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset
 
-    @action(methods=["POST"], detail=False)
+    @action(methods=["POST"], detail=False, serializer_class=WebsocketConnectionSerializer)
     def connect(self, request, *args, **kwargs):
-        print(request.data)
-        return Response(status=200)
+        serializer = self.get_serializer(
+            data={
+                "id": request.data["id"],
+                "access_token": request.data["query_params"].get("access_token"),
+            }
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=201)
 
-    @action(methods=["POST"], detail=False)
+    @action(methods=["POST"], detail=False, serializer_class=WebsocketDisconnectionSerializer)
     def disconnect(self, request, *args, **kwargs):
-        print(request.data)
-        return Response()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=201)
