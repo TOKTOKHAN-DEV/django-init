eac47e1304137c4b4aff54a3839fc71ccd82f146
devnine99 <46771072+devnine99@users.noreply.github.com>
2024-07-02 10:25:45 +0900

    main

--- src/app/websocket_connection/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from app.websocket_connection.models import WebsocketConnection
-
-
-@admin.register(WebsocketConnection)
-class WebsocketConnectionAdmin(admin.ModelAdmin):
-    pass
