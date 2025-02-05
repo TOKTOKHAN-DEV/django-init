fe62f21da0822ecbba478bda2b04877b99876e34
devnine99 <46771072+devnine99@users.noreply.github.com>
2025-02-05 14:04:55 +0900

    main

--- src/app/user/v1/serializers.py
+++ src/app/user/v1/serializers.py
@@ -153,14 +153,6 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         register_token = attrs.pop("register_token", None)
-        email = attrs.get("email")
-        email_token = attrs.pop("email_token", None)
-        phone = attrs.get("phone")
-        phone_token = attrs.pop("phone_token", None)
-
-        password = attrs.get("password")
-        password_confirm = attrs.pop("password_confirm", None)
-
         if register_token:
             try:
                 payload = jwt.decode(register_token, key=settings.SECRET_KEY, algorithms=settings.SIMPLE_JWT_ALGORITHM)
@@ -170,43 +162,6 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
             return attrs
 
-        if "email" in User.VERIFY_FIELDS:
-            # 이메일 토큰 검증
-            try:
-                self.email_verifier = EmailVerifier.objects.get(email=email, token=email_token)
-            except EmailVerifier.DoesNotExist:
-                raise ValidationError("이메일 인증을 진행해주세요.")
-        if "email" in User.VERIFY_FIELDS or "email" in User.REGISTER_FIELDS:
-            # 이메일 검증
-            if User.objects.filter(email=email).exists():
-                raise ValidationError({"email": ["이미 가입된 이메일입니다."]})
-
-        if "phone" in User.VERIFY_FIELDS:
-            # 휴대폰 토큰 검증
-            try:
-                self.phone_verifier = PhoneVerifier.objects.get(phone=phone, token=phone_token)
-            except PhoneVerifier.DoesNotExist:
-                raise ValidationError("휴대폰 인증을 진행해주세요.")
-        if "phone" in User.VERIFY_FIELDS or "phone" in User.REGISTER_FIELDS:
-            # 휴대폰 검증
-            if User.objects.filter(phone=phone).exists():
-                raise ValidationError({"phone": ["이미 가입된 휴대폰입니다."]})
-
-        if "password" in User.REGISTER_FIELDS:
-            errors = {}
-            # 비밀번호 검증
-            if password != password_confirm:
-                errors["password"] = ["비밀번호가 일치하지 않습니다."]
-                errors["password_confirm"] = ["비밀번호가 일치하지 않습니다."]
-            else:
-                try:
-                    validate_password(password)
-                except DjangoValidationError as error:
-                    errors["password"] = list(error)
-                    errors["password_confirm"] = list(error)
-            if errors:
-                raise ValidationError(errors)
-
         return attrs
 
     @transaction.atomic
@@ -214,10 +169,6 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(
             **validated_data,
         )
-        if "email" in User.VERIFY_FIELDS:
-            self.email_verifier.delete()
-        if "phone" in User.VERIFY_FIELDS:
-            self.phone_verifier.delete()
 
         refresh = RefreshToken.for_user(user)
 
