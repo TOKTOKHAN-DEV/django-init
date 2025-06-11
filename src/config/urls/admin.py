3acb0daef086b5fd7f5a4bcde59c86c7e44cf2e5
jhwang0801 <jhwang90801@gmail.com>
2025-06-11 10:34:41 +0900

    ✨ Feat: feat: Add to customize the site header, title and index title for ADMIN

--- src/config/urls/admin.py
+++ src/config/urls/admin.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import Group
 from django.urls import include, path
 from django.utils.safestring import mark_safe
 
+# admin.site.site_header = "페이지 상단 헤더 제목 변경(화면에 보이는 큰 어드민 제목)"
+# admin.site.site_title = "브라우저 탭 제목 변경"
+# admin.site.index_title = "메인페이지의 '사이트 관리' 제목 변경"
+
 urlpatterns = [
     path("ckeditor/", include("ckeditor_uploader.urls")),
     path("", admin.site.urls),
