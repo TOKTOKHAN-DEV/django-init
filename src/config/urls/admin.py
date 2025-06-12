04f6e9f57bc6179b550c47916fe7eff71503e849
jhwang0801 <jhwang90801@gmail.com>
2025-06-12 12:40:40 +0900

    🐛 Fix: fix: Fix Urlpatterns in admin.py

--- src/config/urls/admin.py
+++ src/config/urls/admin.py
@@ -11,10 +11,8 @@ from django.utils.safestring import mark_safe
 
 urlpatterns = [
     path("ckeditor/", include("ckeditor_uploader.urls")),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     path("", admin.site.urls),
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 admin.site.unregister(Group)
