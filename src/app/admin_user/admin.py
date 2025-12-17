98e9cc57c9f43b1580bb8834cf5f08911ce5e813
devnine99 <46771072+devnine99@users.noreply.github.com>
2025-12-17 11:58:58 +0900

    main

--- src/app/admin_user/admin.py
+++ src/app/admin_user/admin.py
@@ -51,11 +51,8 @@ class AdminUserAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return request.user.is_superuser
 
-    def has_view_permission(self, request, obj=None):
-        return request.user.is_superuser
-
     def has_change_permission(self, request, obj=None):
-        return request.user.is_superuser
+        return request.user.is_superuser or request.user == obj
 
     def has_delete_permission(self, request, obj=None):
         return request.user.is_superuser and request.user != obj
