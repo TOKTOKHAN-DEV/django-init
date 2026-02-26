c1b4629e14868c91f137244b4c1571c495af4c6a
devnine99 <46771072+devnine99@users.noreply.github.com>
2026-02-26 14:55:51 +0900

    main

--- src/app/common/schedule_client.py
+++ src/app/common/schedule_client.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 class ScheduleClient:
     client = boto3.client("scheduler", region_name="ap-northeast-2")
-    prefix = f"{settings.PROJECT_NAME}-{settings.APP_ENV}-"
+    prefix = f"{settings.PROJECT_NAME}-{settings.APP_ENV}"
 
     def list(self):
         response = self.client.list_schedules(NamePrefix=self.prefix)
@@ -21,10 +21,10 @@ class ScheduleClient:
             FlexibleTimeWindow={"Mode": "OFF"},
             Target={
                 "Arn": f"arn:aws:events:ap-northeast-2:{settings.AWS_ACCOUNT_ID}:event-bus/default",
-                "RoleArn": f"arn:aws:iam::{settings.AWS_ACCOUNT_ID}:role/{settings.PROJECT_NAME}-{settings.APP_ENV}-EventBridgeSchedulerRole",
+                "RoleArn": f"arn:aws:iam::{settings.AWS_ACCOUNT_ID}:role/{self.prefix}-EventBridgeSchedulerRole",
                 "EventBridgeParameters": {
                     "DetailType": "Scheduled Event",
-                    "Source": f"{settings.PROJECT_NAME}-{settings.APP_ENV}.scheduler",
+                    "Source": f"{self.prefix}.scheduler",
                 },
                 "Input": json.dumps({"path": f"schedule/{path}"}),
                 "RetryPolicy": {"MaximumRetryAttempts": 0},
